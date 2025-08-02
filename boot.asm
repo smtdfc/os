01f0b18e91ee315ec7960906e9e1da00169b765d
smtdfc <smtdfc.master@gmail.com>
2025-08-02 20:18:11 +0700

    update

--- boot.asm
+++ boot.asm
@@ -2,12 +2,19 @@
 [ORG 0x7C00]
 
 
-loop:
-  jmp loop
+mov ah, 0x0e ; tty mode
+mov al, 'H'
+int 0x10
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10
+mov al, 'o'
+int 0x10
 
-times 510-($-$$) db 0
+jmp $
 
-dw 0xaa55
-
-
-
+; padding and magic number
+times 510 - ($-$$) db 0
+dw 0xaa55 
