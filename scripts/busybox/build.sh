ee7eebf7acf4627bdbfcf178bb444752a90df256
smtdfc <smtdfc.master@gmail.com>
2025-08-04 12:35:08 +0700

    update

--- scripts/busybox/build.sh
+++ scripts/busybox/build.sh
@@ -5,6 +5,9 @@ set -e
 BUILD_PROC=2
 
 step "Build BusyBox 1.36.1"
+cp ../configs/busybox.config .config
+make olddefconfig   
+
 cd busybox-1.36.1
 make -j$BUILD_PROC
 cp ./busybox ../build/busybox
--- scripts/init.sh
+++ scripts/init.sh
@@ -2,5 +2,7 @@
 set -e
 source scripts/utils.sh
 
+mkdir -p build
+mkdir -p config
 bash scripts/kernel/init.sh
 bash scripts/busybox/init.sh
