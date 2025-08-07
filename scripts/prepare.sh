de489e2675ec622d2133e901b499c36a5ca9c140
smtdfc <smtdfc.master@gmail.com>
2025-08-07 11:08:31 +0700

    update

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,3 +1,7 @@
+echo "Setting up environment "
+mkdir -p build
+
+
 echo "Setting up tools"
 sudo apt update
 sudo apt-get install bzip2 git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-dev syslinux dosfstools nano git
