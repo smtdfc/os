2382034f4332593e9d27a5f6e508c527405f0da1
smtdfc <smtdfc.master@gmail.com>
2025-08-07 10:19:14 +0700

    update

--- scripts/kernel/build.sh
+++ scripts/kernel/build.sh
@@ -1,9 +1,24 @@
 cd linux
+MAKEFILE_PATH=./Makefile
 
 echo "Loading config "
 cp ../config/linux/.config ./.config
 
 make oldconfig
+
+
+if [ ! -f "$MAKEFILE_PATH" ]; then
+  echo "Makefile not found"
+  exit 1
+fi
+
+if grep -q '^EXTRAVERSION' "$MAKEFILE_PATH"; then
+  sed -i 's/^EXTRAVERSION.*/EXTRAVERSION = -smtdfcOS/' "$MAKEFILE_PATH"
+else
+  sed -i '/^SUBLEVEL.*/a EXTRAVERSION = -smtdfcOS' "$MAKEFILE_PATH"
+fi
+
+grep EXTRAVERSION "$MAKEFILE_PATH"
 
 echo "Building Kernel"
 make -j$(nproc)
