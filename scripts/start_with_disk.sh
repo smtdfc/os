a3ddd934dc80ea69413405926678958997e252a4
smtdfc <smtdfc.master@gmail.com>
2025-08-07 15:13:02 +0700

    update

---
+++ scripts/start_with_disk.sh
@@ -0,0 +1,8 @@
+qemu-system-x86_64 \
+  -m 1G \
+  -kernel  build/bzImage \
+  -initrd build/init.cpio \
+  -nographic \
+  -net nic -net user \
+  -append "console=ttyS0 loglevel=7  init=/init"
+  -drive file=disk.img,format=raw,if=virtio \
