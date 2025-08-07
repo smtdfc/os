ff8ed18b33c6d67b10be960b72d2359db9ca80f8
smtdfc <smtdfc.master@gmail.com>
2025-08-07 10:52:26 +0700

    update

--- scripts/prepare.sh
+++ scripts/prepare.sh
@@ -1,4 +1,5 @@
 echo "Setting up tools"
+sudo apt update
 sudo apt-get install bzip2 git vim make gcc libncurses-dev flex bison bc cpio libelf-dev libssl-dev syslinux dosfstools nano git
 
 echo "Cloning Linux Kernel source "
