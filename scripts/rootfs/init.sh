7cbcae6b5fe485d4e4284d6c9379255e2dc05c76
smtdfc <135759660+smtdfc@users.noreply.github.com>
2025-08-08 01:42:22 +0000

    update

--- scripts/rootfs/init.sh
+++ scripts/rootfs/init.sh
@@ -21,14 +21,43 @@ ID=$AUTHOR
 PRETTY_NAME="$OS_NAME 0.1"
 EOF
 
+# ANSI color codes
+RED="\[\033[0;31m\]"
+GREEN="\[\033[0;32m\]"
+YELLOW="\[\033[0;33m\]"
+BLUE="\[\033[0;34m\]"
+MAGENTA="\[\033[0;35m\]"
+CYAN="\[\033[0;36m\]"
+RESET="\[\033[0m\]"
+
+
 cat <<EOF > rootfs/etc/profile
-export PS1='@\u# '
+export PS1="$CYAN@$GREEN\u$WHITE:$BLUE\w$RESET# "
 export PATH="/bin:/sbin:/usr/bin:/usr/sbin:\$PATH"
 EOF
+
+
+
 
 cat <<EOF > rootfs/etc/resolv.conf
 nameserver 8.8.8.8
 nameserver 1.1.1.1
+EOF
+
+cat > rootfs/etc/passwd <<EOF
+root:x:0:0:root:/root:/bin/sh
+user:x:1000:1000:User:/home/user:/bin/sh
+EOF
+
+cat > rootfs/etc/shadow <<EOF
+root::0:0:99999:7:::
+user::0:0:99999:7:::
+EOF
+chmod 600 rootfs/etc/shadow
+
+cat > rootfs/etc/group <<EOF
+root:x:0:
+user:x:1000:
 EOF
 
 
